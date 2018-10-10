03bcb7d375753b871ecf77fa5507962866fa613a
@fabiano.paula <fabiano.rpaula@gmail.com>
2018-10-10 20:01:27 -0300

    Jogo Até o Momento

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+
+    private Camera Principal;
+    private GameObject Jogador;
+    public float velcamm = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+        Principal = GetComponent<Camera>();
+        Jogador = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if ((Jogador.transform.position.x < Principal.transform.position.x - 0.5) )
+        {
+            Vector3 PosJogador = new Vector3(Jogador.transform.position.x-0.5F, Principal.transform.position.y, Principal.transform.position.z);
+            Principal.transform.position = Vector3.MoveTowards(Principal.transform.position, PosJogador, velcamm);
+        }
+        if ((Jogador.transform.position.x < Principal.transform.position.x - 0.5) || (Jogador.transform.position.x > Principal.transform.position.x + 0.5))
+        {
+            Vector3 PosJogador = new Vector3(Jogador.transform.position.x+0.5F, Principal.transform.position.y, Principal.transform.position.z);
+            Principal.transform.position = Vector3.MoveTowards(Principal.transform.position, PosJogador, velcamm);
+        }
+        /*
+        if ( (Jogador.transform.position.x > Principal.transform.position.x + 0.5))
+        {
+            Vector3 PosJogador = new Vector3(Jogador.transform.position.x, Principal.transform.position.y, Principal.transform.position.z);
+            Principal.transform.position = Vector3.MoveTowards(Principal.transform.position, PosJogador, 0.03f);
+        }*/
+    }
+}
---
+++ Assets/Scripts/Turtle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turtle : MonoBehaviour {
+
+    Rigidbody2D Corpo;
+    SpriteRenderer Imagem;
+    float velocidade = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        Corpo = GetComponent<Rigidbody2D>();
+        Imagem = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Corpo.velocity = new Vector2(velocidade * 2, 0);
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Inimigo")
+        {
+            velocidade = velocidade * -1;
+            if (velocidade > 0)
+            {
+                Imagem.flipX = false;
+            }
+            else
+            {
+                Imagem.flipX = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ColisorInimigo")
+        {
+            velocidade = velocidade * -1;
+            if (velocidade > 0)
+            {
+                Imagem.flipX = false;
+            }
+            else
+            {
+                Imagem.flipX = true;
+            }
+        }
+        
+
+    }
+
+}
