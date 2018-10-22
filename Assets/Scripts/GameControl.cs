eeffe1e0d2e252ab28430fbc9e381473b86e37af
@fabiano.paula <fabiano.rpaula@gmail.com>
2018-10-22 19:25:25 -0300

    #Atualização

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+
+    public GameObject Gameover;
+    public Text Score;
+    public bool pause = false;
+    private int pontos = 0;
+
+
+
+
+    public void Pontuar(int ponto)
+    {
+        pontos = pontos + ponto;
+        Score.text = "SCORE: " + pontos.ToString();
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 1;
+        //pause = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void Iniciar()
+    {
+        Time.timeScale = 1;
+        pause = false;
+    }
+
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        Gameover.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/MarioT.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioT : MonoBehaviour {
+
+    //Variavel De Fisica
+    private Rigidbody2D Corpo;
+    private Animator Animar;
+    private SpriteRenderer Spritemario;
+    private float velocidade = 0;
+    public float acel = -1;
+    public float forcapulo = 0;
+    private bool nochao = false;
+
+    public GameObject Casco;
+
+    void Start()
+    {
+        //Recebe o componete de Fisica
+        Corpo = GetComponent<Rigidbody2D>();
+        Animar = GetComponent<Animator>();
+        Spritemario = GetComponent<SpriteRenderer>();
+
+    }
+
+
+    void Update()
+    {
+        //Chama a função de andar
+        Andar();
+        Pular();
+        //Chao();
+
+    }
+    void Pular()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (nochao == false)
+            {
+
+                forcapulo = 3;
+                nochao = true;
+
+                Animar.SetBool("Pulo", true);
+
+            }
+        }
+        if (nochao == true)
+        {
+            if (forcapulo < 0)
+            {
+                forcapulo = forcapulo + (acel / 2);
+
+
+            }
+            else
+            {
+                forcapulo = forcapulo + acel;
+
+            }
+        }
+    }
+    //Função de Andar
+    void Andar()
+    {
+
+
+        velocidade = Input.GetAxis("Horizontal");
+        Corpo.velocity = new Vector2(velocidade * 2, forcapulo);
+
+        if (velocidade != 0)
+        {
+            Animar.SetFloat("Mover", 2);
+            if (velocidade > 0)
+            {
+                Spritemario.flipX = false;
+            }
+            else
+            {
+                Spritemario.flipX = true;
+            }
+        }
+        else
+        {
+            Animar.SetFloat("Mover", 0);
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+    void Chao()
+    {
+
+        int layerMask = 1 << 12; // Layers utilizadas
+        ///O Ponto de Saida
+        float rodax = -0.035f; //essa variavel ajuda a rodar os pontos do pé
+        float px = 0; //ponto de origem X
+        float py = this.transform.position.y; // ponto de origem Y
+
+        while (rodax < 0.035f)
+        {
+            //lança raios no chão
+            RaycastHit2D hit = new RaycastHit2D();
+            //vai mudando a posição
+            px = this.transform.position.x + rodax;
+            //receber colisão
+            //recebe colisão - ponto inicial - direcção - distancia - camada
+            hit = Physics2D.Raycast(new Vector2(px + rodax, py), Vector3.down, 0.17f, layerMask);
+            Debug.DrawLine(new Vector3(px + rodax, py, 0), new Vector3(px + rodax, py - 0.17f, 0), Color.red);
+            //incrementa
+            rodax = rodax + 0.004f;
+            //Retorna se encontrou algo
+            if (hit == true)
+            {
+                if (hit.collider.tag == "Inimigo")
+                {
+                    //Criar antes Public GameObject Casco no inicio do Codigo
+                    //Cria um Casco no Momento da morte do inimigo
+                    Instantiate(Casco, hit.collider.gameObject.transform.position, Quaternion.identity);
+                    //Destroi o Inimigo
+                    Destroy(hit.collider.gameObject);
+                    //Adicionar uma força de pulo
+                    forcapulo = 1;
+                }
+                if (hit.collider.tag == "Chao")
+                {
+
+
+
+                    nochao = false;
+                    //Animar.SetBool("Pulo", false);
+
+                }
+                if (hit.collider.tag == "Casco")
+                {
+
+
+                    nochao = false;
+
+                }
+                //sai do loop se já tiver encontrado o chão
+                break;
+            }
+            else
+            {
+                Debug.Log("voando");
+                nochao = true;
+                Animar.SetBool("Pulo", false);
+                //enautno não encontra chão fica como false
+                //nochao = false;
+            }
+        }
+    }
+
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Chao")
+        {
+            if (nochao == true)
+            {
+                nochao = false;
+                forcapulo = 0;
+            }
+        }
+
+        if (col.gameObject.tag == "Inimigo")
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameControl>().GameOver();
+
+
+        }
+        if (col.gameObject.tag == "Moeda")
+        {
+
+            Destroy(col.gameObject);
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameControl>().Pontuar(1);
+
+        }
+
+    }
+}
